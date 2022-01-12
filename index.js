39230f5e7c3b03a0e3e9b5ee159768e2114e9ec4
sserra16 <sserradev@gmail.com>
2022-01-12 12:28:32 -0300

    Final commit

--- index.js
+++ index.js
@@ -1,58 +1,137 @@
-/* Pegando o valor do input */
-const btt_bill = document.querySelector('.invisible')
-const btt_number = document.querySelector('.invisible2')
-value_bill = 0
-value_number = 0
-value_tip = 0
+const bill = document.getElementById('input_bill')
+const btt = document.querySelectorAll('.tip')
+const results = document.querySelectorAll('.number_tip')
+const custom = document.querySelector('.input_custom')
+const people = document.getElementById('input_number')
+const btt_reset = document.querySelector('.btt')
+const error_msg = document.querySelector('.error')
 
-btt_bill.addEventListener('click', function (e) {
-    e.preventDefault();
-    const bill = document.querySelector('#input_bill')
-    value_bill = parseFloat(bill.value)
-})
+let bill_value = 0;
+let tip_value = 0;
+let people_value = 0;
 
-btt_number.addEventListener('click', function(e){
-    e.preventDefault()
-    const number = document.querySelector('#input_number')
-    value_number = parseFloat(number.value)
-    value_tip = (count_perc / value_number).toFixed(2)
-})
+function validateFloat(s) {
+    var rgx = /^[0-9]*\.?[0-9]*$/;
+    return s.match(rgx);
+};
 
-/* Deixando os botões marcados */
-const btt = document.querySelectorAll('.btt_perc')
+function validadeInt(s) {
+    var rgx = /^[0-9]*$/;
+    return s.match(rgx);
+};
 
-function checked_btt(e) {
-    e.onclick = function (e) {
-        e.preventDefault()
+function calculateTip() {
+    if(people_value >= 1) {
+        let tipAmount = bill_value * tip_value / people_value;
+        let total = bill_value * (tip_value + 1) / people_value;
+        results[0].innerHTML = '$' + tipAmount.toFixed(2);
+        results[1].innerHTML = '$' + total.toFixed(2);
+    } else {
+        results[0].innerHTML = '$0.00'
+        results[1].innerHTML = '$0.00'
     }
 }
 
-btt.forEach(checked_btt)
+function handleClick(event) {
+    event.preventDefault()
+    btt.forEach(btn => {
+        btn.classList.remove('checked');
 
-/* Fazendo as contas */
-let count_perc = 0
+        if(event.target.innerHTML == btn.innerHTML) {
+            btn.classList.add('checked');
+            tip_value = parseFloat(btn.innerHTML) / 100; //%
+        }
+    });
 
-btt[0].onclick = function(e){
-    e.preventDefault()
-    count_perc = (value_bill * 5) / 100
-}
-btt[1].onclick = function(e){
-    e.preventDefault()
-    count_perc = (value_bill * 10) / 100
-}
-btt[2].onclick = function(e){
-    e.preventDefault()
-    count_perc = (value_bill * 15) / 100
-}
-btt[3].onclick = function(e){
-    e.preventDefault()
-    count_perc = (value_bill * 25) / 100
-}
-btt[4].onclick = function(e){
-    e.preventDefault()
-    count_perc = (value_bill * 50) / 100
+    custom.value = '';
+    calculateTip();
 }
 
+function setBillValue() {
+    if(bill.value.includes(',')) {
+        bill.value = bill.value.replace(',', '.');
+    }
 
+    if(!validateFloat(bill.value)) {
+        bill.value = bill.value.substring(0, bill.value.length-1);
+    }
 
+    bill_value = parseFloat(bill.value);
 
+    calculateTip();
+
+    emptyResetColor();
+}
+
+function setCustomValue() {
+    if(!validadeInt((custom.value))){
+        custom.value = custom.value.substring(0, custom.value.length-1);
+    };
+
+    tip_value = parseFloat(custom.value) / 100
+
+    btt.forEach(btt => {
+        btt.classList.remove('checked')
+    });
+
+    if(custom.value !== '') {
+        calculateTip()
+    }
+
+    emptyResetColor()
+}
+
+function setPeopleValue() {
+    if(!validadeInt(people.value)) {
+        people.value = people.value.substring(0, people.value.length-1);
+    }
+
+    people_value = parseInt(people.value)
+
+    if(people_value <= 0) {
+        people.classList.add('error-wrapper')
+        error_msg.classList.add('error-v')
+        setTimeout(function() {
+            people.classList.remove('error-wrapper')
+        error_msg.classList.remove('error-v')
+        }, 2500);
+    }
+
+    calculateTip()
+
+    emptyResetColor()
+}
+
+function emptyResetColor() {
+    if(bill.value === '' && custom.value === bill.value && custom.value === people.value) {
+        btt_reset.classList.remove('active')
+    } else {
+        btt_reset.classList.add('active')
+    }
+}
+
+function reset() {
+    bill.value = ''
+    setBillValue()
+
+    custom.value = '';
+    setCustomValue();
+
+    people.value = '';
+    setPeopleValue();
+
+    results[0].innerHTML = '$0.00';
+    results[1].innerHTML = '$0.00';
+}
+
+bill.addEventListener('input', setBillValue);
+
+btt.forEach(btn => {
+    btn.addEventListener('click', handleClick)
+});
+
+custom.addEventListener('input', setCustomValue);
+
+people.addEventListener('input', setPeopleValue);
+
+btt_reset.addEventListener('click', reset);
